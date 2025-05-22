a9877471117fc482749a3d326b00d99a79650482
spacegirl298 <jessicajardim2004@gmail.com>
2025-05-22 14:53:46 +0200

    back to top

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  });
+
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
